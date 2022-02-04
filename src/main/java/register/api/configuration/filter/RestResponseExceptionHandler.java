ba5a2bce24e31e5218d4bbc79deccd474b2b5590
m.saberi <maryamsaberi1994@yahoo.com>
2022-02-04 13:04:54 +0330

    add exception handling

---
+++ src/main/java/register/api/configuration/filter/RestResponseExceptionHandler.java
@@ -0,0 +1,60 @@
+package register.api.configuration.filter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import register.api.model.RestResponse;
+import register.exception.UserException;
+import register.exception.UserExceptionStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Slf4j
+@ControllerAdvice
+public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler({UserException.class})
+    public ResponseEntity<?> userExceptionHandler(HttpServletRequest request, UserException exception) throws JsonProcessingException {
+
+        UserExceptionStatus status = exception.getStatus();
+        return ResponseEntity
+                .status(status.getCode())
+                .body(new RestResponse<>(
+                        status.getCode(),
+                        status.getReasonPhrase(),
+                        (String) request.getAttribute("userUri"),
+                        null,
+                        exception.getMessage()
+                ));
+    }
+
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<?> runtimeExceptionHandler(HttpServletRequest request, RuntimeException exception) throws JsonProcessingException {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String userUri = (String) request.getAttribute("userUri");
+        String referenceId = (String) request.getAttribute("referenceId");
+
+        log.error("5xx exception with uri: " + userUri + " ,reff: " + referenceId, exception);
+
+        return ResponseEntity
+                .status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new RestResponse<>(
+                        status.value(),
+                        status.getReasonPhrase(),
+                        userUri,
+                        "مشکلی در پردازش به وجود آمده است."
+                ));
+    }
+
+
+}
